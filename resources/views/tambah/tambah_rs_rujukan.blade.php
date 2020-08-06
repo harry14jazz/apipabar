22fe543a94232b5bb7383eaee3b0907099909df7
Muhammad Nursalli <shallimuhammad@gmail.com>
2020-08-06 13:31:28 +0700

    toggle menu

--- resources/views/tambah/tambah_rs_rujukan.blade.php
+++ resources/views/tambah/tambah_rs_rujukan.blade.php
@@ -2,7 +2,7 @@
 
     <div class="container-fluid">
       <div class="row">
-        <nav class="col-md-2 d-none d-md-block bg-light sidebar">
+        <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
           <div class="sidebar-sticky">
             <ul class="nav flex-column">
               <li class="nav-item">
